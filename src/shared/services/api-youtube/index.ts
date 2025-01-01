b77b960fc8efb031ae0c5a3840f0d2172c5cf2fe
Tiago Lopes <tiagoltavares2002@gmail.com>
2025-01-01 18:12:10 -0300

    chore: configurando cache das requisições

--- src/shared/services/api-youtube/index.ts
+++ src/shared/services/api-youtube/index.ts
@@ -1,18 +1,35 @@
 import { youtube } from '@googleapis/youtube'
 
+const fetchWithNextConfig =
+  (nextConfig?: NextFetchRequestConfig): typeof fetch =>
+  (input, params = {}) => {
+    return fetch(input, {
+      ...params,
+      ...nextConfig,
+    })
+  }
+
 const YoutubeApiClient = youtube({
   version: 'v3',
   auth: process.env.YOUTUBE_API_KEY,
+  fetchImplementation: fetchWithNextConfig(),
 })
 
 export const APIYoutube = {
   course: {
     getAll: async () => {
-      const { data } = await YoutubeApiClient.playlists.list({
-        maxResults: 50,
-        part: ['snippet'],
-        channelId: process.env.YOUTUBE_CHANNEL_ID,
-      })
+      const { data } = await YoutubeApiClient.playlists.list(
+        {
+          maxResults: 50,
+          part: ['snippet'],
+          channelId: process.env.YOUTUBE_CHANNEL_ID,
+        },
+        {
+          fetchImplementation: fetchWithNextConfig({
+            revalidate: 60 * 60 * 48,
+          }),
+        }
+      )
 
       const courses =
         data.items?.map((item) => {
